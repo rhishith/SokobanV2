5ad41ce7eb3037d8eb8e4c9deb3d02cb1400595d
Rhishith <rhishithk@gmail.com>
2025-12-07 20:23:02 +0530

    Player movement changed to input action

--- Assets/My Assets/Scripts/PlayerController.cs
+++ Assets/My Assets/Scripts/PlayerController.cs
@@ -1,63 +1,120 @@
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public AudioManager audiomanager;
-    public GridManager gridManager;
-    public Vector2Int playerPos;
-    public LevelLoader levelLoader;
-    public Sprite[] characterViews;
-    private SpriteRenderer spriteRenderer;
-    private Stack<Tile[,]> undoStack = new Stack<Tile[,]>();
+    [Header("Settings")]
+    [SerializeField] private Sprite[] characterViews; // 0: Up, 1: Down, 2: Left, 3: Right
+    
+    [Header("Input")]
+    [SerializeField] private InputActionAsset inputActionAsset;
 
-    private void Start()
+    private AudioManager audioManager;
+    private GridManager gridManager;
+    private Vector2Int playerPos;
+    
+    private SpriteRenderer spriteRenderer;
+    private InputActionMap playerMap;
+    private InputAction moveAction;
+    private InputAction undoAction; // If you want to add undo later
+    private InputAction restartAction; // "
+
+    private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (inputActionAsset != null)
+        {
+            playerMap = inputActionAsset.FindActionMap("Player");
+            if (playerMap != null)
+            {
+                moveAction = playerMap.FindAction("Move");
+            }
+        }
     }
 
-    void Update()
+    public void Initialize(GridManager grid, AudioManager audioMgr, Vector2Int startPos)
     {
-        if (gridManager == null || gridManager.grid == null) return;
-        Vector2Int dir = Vector2Int.zero;
-
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            dir = Vector2Int.up;
-            spriteRenderer.sprite = characterViews[0];
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            dir = Vector2Int.down;
-            spriteRenderer.sprite = characterViews[1];
-        }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            dir = Vector2Int.left;
-            spriteRenderer.sprite = characterViews[2];
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            dir = Vector2Int.right;
-            spriteRenderer.sprite = characterViews[3];
-        }
-
-        if (dir != Vector2Int.zero)
-            TryMove(dir);
-
-        if (Input.GetKeyDown(KeyCode.U))
-        {
-            UndoMove();
-            audiomanager.PlayUndoSfx();
-        }
-        if (Input.GetKeyDown(KeyCode.R)) RestartLevel();
+        gridManager = grid;
+        audioManager = audioMgr;
+        playerPos = startPos;
+        transform.position = gridManager.GridToWorld(playerPos); // Set initial visual position
     }
 
-    void TryMove(Vector2Int dir)
+    private void OnEnable()
     {
-        //Debug.Log("Trying to move: " + dir);
+        if (playerMap != null) playerMap.Enable();
+        if (moveAction != null) moveAction.performed += OnMove;
+    }
+
+    private void OnDisable()
+    {
+        if (playerMap != null) playerMap.Disable();
+        if (moveAction != null) moveAction.performed -= OnMove;
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        if (gridManager == null || gridManager.Grid == null) return;
+        if (!GameManager.Instance.IsPlayerInputEnabled) return;
+
+        Vector2 input = context.ReadValue<Vector2>();
+
+        // Filter small values (deadzone/noise)
+        if (input.sqrMagnitude > 0.5f)
+        {
+            Vector2Int dir = GetDirectionFromInput(input);
+            if (dir != Vector2Int.zero)
+            {
+                TryMove(dir);
+            }
+        }
+    }
+
+    private Vector2Int GetDirectionFromInput(Vector2 input)
+    {
+        if (Mathf.Abs(input.y) > Mathf.Abs(input.x))
+        {
+            // Vertical movement
+            if (input.y > 0)
+            {
+                SetSprite(0);
+                return Vector2Int.up;
+            }
+            else
+            {
+                SetSprite(1);
+                return Vector2Int.down;
+            }
+        }
+        else
+        {
+            // Horizontal movement
+            if (input.x < 0)
+            {
+                SetSprite(2);
+                return Vector2Int.left;
+            }
+            else
+            {
+                SetSprite(3);
+                return Vector2Int.right;
+            }
+        }
+    }
+
+    private void SetSprite(int index)
+    {
+        if (characterViews != null && index >= 0 && index < characterViews.Length)
+        {
+            spriteRenderer.sprite = characterViews[index];
+        }
+    }
+
+    private void TryMove(Vector2Int dir)
+    {
         Vector2Int targetPos = playerPos + dir;
+        
         if (!gridManager.IsInsideGrid(targetPos)) return;
 
         Tile targetTile = gridManager.GetTile(targetPos);
@@ -65,121 +122,83 @@ public class PlayerController : MonoBehaviour
 
         if (targetTile.Type == TileType.Wall) return;
 
-        if (targetTile.Occupier != null && targetTile.Occupier.CompareTag("Box") && GameManager.instance.playerCanMove)
+        // Check for box interaction
+        if (targetTile.Occupier != null && targetTile.Occupier.CompareTag("Box"))
         {
-            // Try to push box
-            Vector2Int boxTarget = targetPos + dir;
-            if (!gridManager.IsInsideGrid(boxTarget)) return;
-
-            Tile boxTargetTile = gridManager.GetTile(boxTarget);
-            if (boxTargetTile.Type != TileType.Wall && boxTargetTile.Occupier == null)
+            if (TryPushBox(targetPos, dir))
             {
-                SaveUndoState();
-                // Move box
-                boxTargetTile.Occupier = targetTile.Occupier;
-                targetTile.Occupier.transform.position = gridManager.GridToWorld(boxTarget);
-                targetTile.Occupier = null;
-                gridManager.SetTile(boxTarget, boxTargetTile);
-                gridManager.SetTile(targetPos, targetTile);
-
-                //sfx
-                if (boxTargetTile.Type == TileType.Goal)
-                {
-                    audiomanager.PlayGoalSfx();
-                }
-                else
-                {
-                    audiomanager.PlayBoxMoveSfx();
-                }
-                // Move player
-                currentTile.Occupier = null;
-                gridManager.SetTile(playerPos, currentTile);
-                playerPos = targetPos;
-                transform.position = gridManager.GridToWorld(playerPos);
-                Tile newPlayerTile = gridManager.GetTile(playerPos);
-                newPlayerTile.Occupier = gameObject;
-                gridManager.SetTile(playerPos, newPlayerTile);
-
+                MovePlayer(targetPos, currentTile);
                 CheckWin();
             }
-            return;
         }
-
-        if (targetTile.Occupier == null && GameManager.instance.playerCanMove)
+        else if (targetTile.Occupier == null)
         {
-            SaveUndoState();
-            currentTile.Occupier = null;
-            gridManager.SetTile(playerPos, currentTile);
-            playerPos = targetPos;
-            transform.position = gridManager.GridToWorld(playerPos);
-            Tile newPlayerTile = gridManager.GetTile(playerPos);
-            newPlayerTile.Occupier = gameObject;
-            gridManager.SetTile(playerPos, newPlayerTile);
-            audiomanager.PlayPlayerMovementSfx();
+            MovePlayer(targetPos, currentTile);
+            audioManager.PlayPlayerMovementSfx();
             CheckWin();
         }
     }
 
-
-
-    void SaveUndoState()
+    private bool TryPushBox(Vector2Int boxPos, Vector2Int dir)
     {
-        Tile[,] snapshot = gridManager.grid.Clone() as Tile[,];
-        undoStack.Push(snapshot);
+        Vector2Int boxTargetPos = boxPos + dir;
+
+        if (!gridManager.IsInsideGrid(boxTargetPos)) return false;
+
+        Tile boxTargetTile = gridManager.GetTile(boxTargetPos);
+
+        // Cannot push into wall or another object
+        if (boxTargetTile.Type == TileType.Wall || boxTargetTile.Occupier != null) 
+            return false;
+
+        // Perform Box Move
+        Tile currentBoxTile = gridManager.GetTile(boxPos);
+        GameObject boxObj = currentBoxTile.Occupier;
+
+        // Update Box Logical Position
+        boxTargetTile.Occupier = boxObj;
+        gridManager.SetTile(boxTargetPos, boxTargetTile);
+
+        currentBoxTile.Occupier = null; // Clear old tile (will be overwritten by player anyway, but good for safety)
+        gridManager.SetTile(boxPos, currentBoxTile);
+
+        // Update Box Visual Position
+        boxObj.transform.position = gridManager.GridToWorld(boxTargetPos);
+
+        // SFX
+        if (boxTargetTile.Type == TileType.Goal)
+        {
+            audioManager.PlayGoalSfx();
+        }
+        else
+        {
+            audioManager.PlayBoxMoveSfx();
+        }
+
+        return true;
     }
 
-    void UndoMove()
+    private void MovePlayer(Vector2Int targetPos, Tile currentTile)
     {
-        if (undoStack.Count == 0) return;
-        gridManager.grid = undoStack.Pop();
-        ResyncSceneFromGrid();
+        // Clear old tile
+        currentTile.Occupier = null;
+        gridManager.SetTile(playerPos, currentTile);
+
+        // Update Logic
+        playerPos = targetPos;
+        Tile newPlayerTile = gridManager.GetTile(playerPos);
+        newPlayerTile.Occupier = gameObject;
+        gridManager.SetTile(playerPos, newPlayerTile);
+
+        // Update Visual
+        transform.position = gridManager.GridToWorld(playerPos);
     }
 
-    void RestartLevel()
-    {
-        // Optionally reload level from LevelLoader
-    }
-
-    void CheckWin()
+    private void CheckWin()
     {
         if (gridManager.AreAllGoalsCovered())
         {
-            StartCoroutine(WaitAndMoveToNextLevel());
-        }
-    }
-
-    private IEnumerator WaitAndMoveToNextLevel()
-    {
-        GameManager.instance.playerCanMove = false;
-        yield return new WaitForSeconds(0.5f);
-        GameManager.instance.currentLevelText.text = $"Level {GameManager.instance.currentLevelIndex + 2}";
-        GameManager.instance.loadingImage.SetActive(true);
-        audiomanager.StopBackgroundMusic();
-        yield return new WaitForSeconds(GameManager.instance.levelWaitTime);
-        GameManager.instance.loadingImage.SetActive(false);
-        levelLoader.NextLevel();
-        GameManager.instance.playerCanMove = true;
-    }
-
-    void ResyncSceneFromGrid()
-    {
-        for (int x = 0; x < gridManager.width; x++)
-        {
-            for (int y = 0; y < gridManager.height; y++)
-            {
-                Vector2Int pos = new Vector2Int(x, y);
-                Tile t = gridManager.GetTile(pos);
-                if (t.Occupier == null) continue;
-                if (t.Occupier.CompareTag("Player"))
-                {
-                    playerPos = pos;
-                    transform.position = gridManager.GridToWorld(playerPos);
-                }
-                else
-                {
-                    t.Occupier.transform.position = gridManager.GridToWorld(pos);
-                }
-            }
+            GameManager.Instance.LevelComplete();
         }
     }
 }
